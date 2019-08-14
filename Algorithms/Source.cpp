e7130d434beb10efedb32d75548ab2efbfc353f0
PC88 <32823980+PC88@users.noreply.github.com>
2019-08-15 00:09:46 +0100

    More detail added later, mathematical concept of induction.

--- Algorithms/Source.cpp
+++ Algorithms/Source.cpp
@@ -1,5 +1,14 @@
 #include "iostream"
+// some algorithmic example from the book, for didactic purposes.
 
+int Multiply0(int n, int a)
+{
+	if (n == 1)
+	{
+		return a;
+	}
+	return Multiply0(n - 1, a) + a;
+}
 
 int main()
 {
